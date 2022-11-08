14ba0cbfd622e8b6023137ac503e7a018e59d426
Norbert Jaśniewicz <norbert.jasniewicz@protonmail.com>
2022-11-08 01:41:11 +0100

    Cleanup

--- compile.sh
+++ compile.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 lex zad1.l
-clang lex.yy.c -o zad1
+clang lex.yy.c -o zad1.bin
 lex zad2.l
-clang lex.yy.c -o zad2
+clang lex.yy.c -o zad2.bin
 lex zad3.l
-clang lex.yy.c -o zad3
+clang lex.yy.c -o zad3.bin
 lex zad4.l
-clang lex.yy.c rev_pol_calc.c -o zad4 -lm
+clang lex.yy.c rev_pol_calc.c -o zad4.bin -lm
+rm lex.yy.c
